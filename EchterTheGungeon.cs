7cfa65394b7849c9ed960bc63278751877ac0010
Stevie <xxlennygamerxx@gmail.com>
2020-12-18 22:20:52 -0800

    Update EchterTheGungeon.cs

--- EchterTheGungeon.cs
+++ EchterTheGungeon.cs
@@ -1,13 +1,17 @@
-﻿namespace EchterTheGungeon
+﻿using System;
+
+namespace EchterTheGungeon
 {
     public class EchterTheGungeon : ETGModule
     {
-        public override void Start()
-        {
-        }
-
         public override void Init()
         {
+            Console.WriteLine("Initialized (EchterTG)");
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Started (EchterTG)");
         }
 
         public override void Exit()
